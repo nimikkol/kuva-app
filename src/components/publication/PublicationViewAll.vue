b039e76661816979dd4472a39b125859cfa3ed93
Niko Mikkola <niko.mikkola2@edu.lapinamk.fi>
2022-04-18 11:36:57 +0300

    first commit

---
+++ src/components/publication/PublicationViewAll.vue
@@ -0,0 +1,15 @@
+<script setup>
+const message ="Testi viesti"
+</script>
+
+<template>
+  <h1>PublicationViewAll OK!</h1>
+    <p>{{ message }}</p>
+</template>
+
+<style>
+body{
+    margin: 30px;
+}
+
+</style>
